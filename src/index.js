032a1e9f137aada6aefa34ca872fdee85c5245db
David Burles <dburles@gmail.com>
2018-04-25 18:04:48 +1000

    add react-hot-loader

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
+import App from './containers/App';
 
-render(<p>Hello</p>, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
