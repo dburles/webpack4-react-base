83404e42f1294745c9171c5af3e0851d7aa9462b
David Burles <dburles@gmail.com>
2018-04-25 18:16:00 +1000

    eval-source-map

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,7 +4,7 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'development',
-  devtool: 'source-map',
+  devtool: 'eval-source-map',
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     open: false,
